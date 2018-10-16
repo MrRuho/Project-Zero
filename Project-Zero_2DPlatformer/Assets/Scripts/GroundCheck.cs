eff6c6e8d292b194a26a5172f877e343086e4556
Paajanen Ville <paajanen.ville@gmail.com>
2018-10-16 20:17:02 +0300

    Animaatiot laitettu toimintaan.

---
+++ Project-Zero_2DPlatformer/Assets/Scripts/GroundCheck.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour {
+
+    private Player player;
+
+    //Note6. Tunnistaa sen koskettaako pelaaja maata vai ei. 
+    private void Start()
+    {
+        player = gameObject.GetComponentInParent<Player>(); 
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        player.grounded = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        player.grounded = false;
+    }
+
+}
