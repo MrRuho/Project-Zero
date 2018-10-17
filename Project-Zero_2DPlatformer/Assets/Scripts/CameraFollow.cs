db151779e8348350c2819ba1113e8b9b069b81dc
Paajanen Ville <paajanen.ville@gmail.com>
2018-10-17 20:16:02 +0300

    Kamera seuraa ylos ja alas.

--- Project-Zero_2DPlatformer/Assets/Scripts/CameraFollow.cs
+++ Project-Zero_2DPlatformer/Assets/Scripts/CameraFollow.cs
@@ -7,6 +7,11 @@ public class CameraFollow : MonoBehaviour {
     public float smoothTimeY;
     public float smoothTimeX;
 
+    public bool bounds;
+
+    public Vector3 minCameraPos;
+    public Vector3 maxCameraPos;
+
     private Vector2 velocity;
 
     public GameObject player;
@@ -24,5 +29,13 @@ public class CameraFollow : MonoBehaviour {
         float posY = Mathf.SmoothDamp(transform.position.y, player.transform.position.y, ref velocity.y, smoothTimeY);
 
         transform.position = new Vector3(posX, posY, transform.position.z);
+
+       if(bounds)
+        {
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x, minCameraPos.x, maxCameraPos.x),
+                Mathf.Clamp(transform.position.y, minCameraPos.y, maxCameraPos.y),
+                Mathf.Clamp(transform.position.z, minCameraPos.z, maxCameraPos.z));
+        }
     }
+
 }
