48a8024b445c96722850c8dc001e43128494c333
Paajanen Ville <paajanen.ville@gmail.com>
2018-10-18 19:08:45 +0300

    Health bar laitettu toimiin.

--- Project-Zero_2DPlatformer/Assets/Scripts/HUD.cs
+++ Project-Zero_2DPlatformer/Assets/Scripts/HUD.cs
@@ -1,16 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HUD : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Sprite[] HeartSprites;
+
+    public Image HeartsUI;
+
+    private Player player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+    }
+
+    void Update()
+    {
+        HeartsUI.sprite = HeartSprites[player.curHealth];   
+    }
 }
