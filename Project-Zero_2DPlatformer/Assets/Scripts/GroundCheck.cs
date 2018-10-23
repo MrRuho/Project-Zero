e827f18b18bd900cf19a56f14ce49ea69cadadff
Paajanen Ville <paajanen.ville@gmail.com>
2018-10-23 20:01:28 +0300

    Bugi korjailuja

--- Project-Zero_2DPlatformer/Assets/Scripts/GroundCheck.cs
+++ Project-Zero_2DPlatformer/Assets/Scripts/GroundCheck.cs
@@ -5,30 +5,31 @@ using UnityEngine;
 public class GroundCheck : MonoBehaviour {
 
     private Player player;
-   
+ 
     //Note6. Tunnistaa sen koskettaako pelaaja maata vai ei. 
     private void Start()
     {
         player = gameObject.GetComponentInParent<Player>();
     }
 
-    void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("TurretRangeCone")) //Estaa pelaajaa tekemasta tuplahyppya turretin rangecolliderin sisalla.
+        /*if (collision.CompareTag("TurretRangeCone")) // Lista Tageista joita taman objektin kuuluu ignoorata. (Ilman tata, luokittelee ne maaksi mahdollistaa esim tuplahypyn.)
         {
             player.grounded = false;
         }
-        else
+        else */
         player.grounded = true;
     }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if (collision.CompareTag("TurretRangeCone")) //Estaa pelaajaa tekemasta tuplahyppya turretin rangecolliderin sisalla.
+
+       /* if (collision.CompareTag("TurretRangeCone"))
         {
             player.grounded = false;
         }
-        else
+        else */
             player.grounded = true;
     }
 
