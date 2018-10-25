5a142c09d61fcc3b3b2d74ba0ece9047d990a4b2
MrRuho <paajanen.ville@gmail.com>
2018-10-25 13:06:28 +0300

    Konfiktit korjattu.
    - Lataan visual studio päivityksen.

--- Project-Zero_2DPlatformer/Assets/Scripts/GroundCheck.cs
+++ Project-Zero_2DPlatformer/Assets/Scripts/GroundCheck.cs
@@ -14,33 +14,21 @@ public class GroundCheck : MonoBehaviour {
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-<<<<<<< HEAD
+
         if (collision.isTrigger == false)  //Estaa pelaajaa tekemasta tuplahyppya turretin rangecolliderin sisalla.
 
             player.grounded = true;
-=======
-        /*if (collision.CompareTag("TurretRangeCone")) // Lista Tageista joita taman objektin kuuluu ignoorata. (Ilman tata, luokittelee ne maaksi mahdollistaa esim tuplahypyn.)
-        {
-            player.grounded = false;
-        }
-        else */
+
         player.grounded = true;
->>>>>>> 7696f349259250598af8e1c8aec5a4bb32e5f037
+
     }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-<<<<<<< HEAD
+
         if (collision.isTrigger == false)
      
-=======
 
-       /* if (collision.CompareTag("TurretRangeCone"))
-        {
-            player.grounded = false;
-        }
-        else */
->>>>>>> 7696f349259250598af8e1c8aec5a4bb32e5f037
             player.grounded = true;
     }
 
