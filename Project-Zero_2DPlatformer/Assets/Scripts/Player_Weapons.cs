4f441b6407f093eb5106f8c5e299edb41fe1e7f5
MrRuho <paajanen.ville@gmail.com>
2018-10-31 14:59:15 +0200

    pointterin kaanto asennettu
    -Huom! Pelin alussa pointteri on väärinpäin mutta kun pelaaja painaa johonkin suuntaan niin se kääntyy oikein päin.

--- Project-Zero_2DPlatformer/Assets/Scripts/Player_Weapons.cs
+++ Project-Zero_2DPlatformer/Assets/Scripts/Player_Weapons.cs
@@ -5,11 +5,31 @@ using UnityEngine;
 public class Player_Weapons : MonoBehaviour {
 
     public Transform firepoint;
-  
+    bool haveTurned = true;
+
+    private void Start()
+    {
+        
+    }
 
     void Update ()
     {
+        if (Input.GetKeyDown("left") && haveTurned == false)
+        {
+            Flip();
+        }
 
-
+        if (Input.GetKeyDown("right") && haveTurned == true)
+        {       
+            Flip();
+        }
+ 
     }
+
+    private void Flip()
+    {
+        haveTurned = !haveTurned;
+        transform.Rotate(0f, 180f, 0f);
+    }
+
 }
