ff4de2ca9229938d70f8eeb0b6dd8d3ffb5da8c3
Paajanen Ville <paajanen.ville@gmail.com>
2018-11-05 10:18:43 +0200

    animationrandomizer

---
+++ Project-Zero_2DPlatformer/Assets/Scripts/AnimationRandomizer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationRandomizer : MonoBehaviour {
+
+
+  public  Animator randomizer;
+	// Use this for initialization
+	void Start ()
+    {
+        randomizer.Play("MushroomIdle", -1, Random.Range(0.0f, 1.0f));
+    }
+	
+	
+}
