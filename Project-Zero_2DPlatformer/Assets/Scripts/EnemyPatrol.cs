9209628f9d517068e9089d4808a628807fbbc47f
MrRuho <paajanen.ville@gmail.com>
2018-11-07 14:29:42 +0200

    zombie enemy tunnistaa seinät ja kääntyy

--- Project-Zero_2DPlatformer/Assets/Scripts/EnemyPatrol.cs
+++ Project-Zero_2DPlatformer/Assets/Scripts/EnemyPatrol.cs
@@ -9,13 +9,15 @@ public class EnemyPatrol : MonoBehaviour {
     private bool movingRight = false;
 
     public Transform groundDetection;
+    public Transform wallDetection;
 
     void Update()
     {
         transform.Translate(Vector2.right * speed * Time.deltaTime);
 
-        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position,Vector2.down, 2f);
-        if (groundInfo.collider == false)
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position,Vector2.down, 0.1f);
+        RaycastHit2D wallInfo = Physics2D.Raycast(wallDetection.position, Vector2.zero, 0.1f);
+        if (groundInfo.collider == false | wallInfo.collider == true)
         {
             if (movingRight == false)
             {
@@ -27,5 +29,6 @@ public class EnemyPatrol : MonoBehaviour {
                 movingRight = false;
             }
         }
+
     }
 }
