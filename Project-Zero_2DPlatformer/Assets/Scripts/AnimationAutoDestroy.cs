dfbfc1f8f5b0ab3ce32241fd6581b4841c6c8d5e
MrRuho <paajanen.ville@gmail.com>
2018-11-08 15:00:03 +0200

    Luodin tuhoutuminen korjattu

--- Project-Zero_2DPlatformer/Assets/Scripts/AnimationAutoDestroy.cs
+++ Project-Zero_2DPlatformer/Assets/Scripts/AnimationAutoDestroy.cs
@@ -6,29 +6,12 @@ public class AnimationAutoDestroy : MonoBehaviour {
 
     public float delay = 0f;
 
-    public GameObject destroyThisGameObject;
-    bool animationPlayed = false;
+   
 
     // Use this for initialization
     void Start()
     {
-      /*  Destroy(gameObject, this.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).length + delay);
-        {
-            animationPlayed = true;
-        }*/
+        Destroy(gameObject, GetComponentInChildren<Animator>().GetCurrentAnimatorStateInfo(0).length + delay);
+    }
 
-        
-    }
-    private void Update()
-    {
-        Destroy(gameObject, this.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).length + delay);
-        {
-            animationPlayed = true;
-        }
-        if (animationPlayed)
-        {
-            Destroy(destroyThisGameObject);
-        }
-    }
-    
 }
