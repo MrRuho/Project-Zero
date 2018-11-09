09eebad80c7851dc23a8971b5e90602658f8a8a0
Paajanen Ville <paajanen.ville@gmail.com>
2018-11-09 11:25:22 +0200

    NimiGenraattori toimii debugis.

---
+++ Project-Zero_2DPlatformer/Assets/Scripts/NameGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NameGenerator : MonoBehaviour {
+
+   public  List<string> names;
+   public  string[] lines;
+
+	// Use this for initialization
+	void Start ()
+    {
+        TextAsset nameText = Resources.Load<TextAsset>("Names");
+
+        lines = nameText.text.Split("\n"[0]);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(50, 50, 50, 50), "generate name"))
+        {
+            Debug.Log(lines[Random.Range(0, lines.Length)]);
+        }
+    }
+}
