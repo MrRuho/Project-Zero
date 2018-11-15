59387671265d21f696a89dd10fa0b590d56ed383
MrRuho <paajanen.ville@gmail.com>
2018-11-15 15:54:47 +0200

    zombien melee työn alla

--- Project-Zero_2DPlatformer/Assets/Scripts/EnemyPatrol.cs
+++ Project-Zero_2DPlatformer/Assets/Scripts/EnemyPatrol.cs
@@ -10,10 +10,12 @@ public class EnemyPatrol : MonoBehaviour {
     private bool getHit = false;
     public Transform groundDetection;
     public Transform wallDetection;
+    public Transform meleeAttackZone;
 
+ 
     void Update()
     {
-        
+     
 
         if (getHit == false)
         {
@@ -26,21 +28,44 @@ public class EnemyPatrol : MonoBehaviour {
 
         RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position,Vector2.down, 0.1f);
         RaycastHit2D wallInfo = Physics2D.Raycast(wallDetection.position, Vector2.zero, 0.1f);
+     
+        Debug.DrawRay(transform.position, Vector2.down, Color.white);
+        Debug.DrawRay(transform.position, Vector2.zero, Color.yellow);
+        
         if (getHit == false)
         {
+            AttackDirectionControl();
             if (groundInfo.collider == false | wallInfo.collider == true)
             {
+               
                 if (movingRight == false)
                 {
                     transform.eulerAngles = new Vector3(0, -180, 0);
                     movingRight = true;
+                    
                 }
                 else
                 {
                     transform.eulerAngles = new Vector3(0, 0, 0);
                     movingRight = false;
+                    
                 }
             }
+        }
+    }
+
+    void AttackDirectionControl()
+    {
+        if (movingRight == true)
+        {
+            RaycastHit2D meleeInfo = Physics2D.Raycast(meleeAttackZone.position, Vector2.right, 0.5f);
+            Debug.DrawRay(transform.position, Vector2.right, Color.red);         
+        }
+        else
+        {
+            RaycastHit2D meleeInfo = Physics2D.Raycast(meleeAttackZone.position, Vector2.left, 0.5f);
+            Debug.DrawRay(transform.position, Vector2.left, Color.red);
+       
         }
     }
 
