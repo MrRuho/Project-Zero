477569d69931cafea1ebf68ecb874c7b3d4277e8
MrRuho <paajanen.ville@gmail.com>
2018-11-29 15:48:03 +0200

    Power Uppeja ja aseenlataus (työvaiheessa.)
    BUGI latauksen aikana voi yliladata asetta.

--- Project-Zero_2DPlatformer/Assets/Scripts/Player_Weapons.cs
+++ Project-Zero_2DPlatformer/Assets/Scripts/Player_Weapons.cs
@@ -4,11 +4,30 @@ using UnityEngine;
 
 public class Player_Weapons : MonoBehaviour {
 
+    private float reloadingTime = 0;
+
+    private int clipSize = 0;
+    private int ammoCounter = 0;
+    private int weaponSwitch = 0;
+    
+    private bool pistol = false;
+    private bool assaultRifle = false;
+    private bool shotgun = false;
+    private bool missile = false;
+    
+
     public Transform firepoint;
     public GameObject bulletPrefab;
 
+    private void Start()
+    {
+        WeaponSwitcher(0);
+    }
+
     void Update ()
     {
+        Debug.Log(ammoCounter);
+        
 
         // Kaantaa ampumapistetta siihen suntaan mihin hahmo katsoo. Estaa nain ampumasta itseaan.
         if (Input.GetKeyDown("left"))
@@ -23,11 +42,62 @@ public class Player_Weapons : MonoBehaviour {
            
         }
  
-        if (Input.GetButtonDown("Fire2"))
+        if (Input.GetButtonDown("Fire2")&& ammoCounter >0)
         {
             Shoot();
+            ammoCounter -= 1;
         }
 
+        if (ammoCounter <= 0)
+        {
+            Debug.Log("Reloading");
+            StartCoroutine(Reloading());
+        }
+
+    }
+
+    IEnumerator Reloading()
+    {
+        yield return new WaitForSeconds(reloadingTime);
+        ammoCounter = clipSize;
+        Debug.Log("Reloated");
+        yield return clipSize;
+    }
+
+    void WeaponSwitcher(int CurrentWeapon)
+    {
+        weaponSwitch = CurrentWeapon;
+
+        switch (weaponSwitch)
+        {
+            case 1:
+                Debug.Log("AssaultRifle");
+                clipSize = 30;
+                ammoCounter = clipSize;
+                reloadingTime = 10.2f;
+                break;
+
+            case 2:
+                Debug.Log("Shotgun");
+                clipSize = 6;
+                ammoCounter = clipSize;
+                reloadingTime = 10.5f;
+                break;
+
+            case 3:
+                Debug.Log("Missile Laucher");
+                clipSize = 3;
+                ammoCounter = clipSize;
+                reloadingTime = 10.8f;
+                break;
+
+            default:
+                Debug.Log("Pistol");
+                clipSize = 6;
+                ammoCounter = clipSize;
+                reloadingTime = 10.0f;
+                break;
+        }
     }
 
     void Shoot()
