f3f27b3db6911cd7e4abd42b2683aa3b870d5f09
Paajanen Ville <paajanen.ville@gmail.com>
2018-12-05 10:03:48 +0200

    pikku muutoksia

--- Project-Zero_2DPlatformer/Assets/Scripts/Player_Bullet.cs
+++ Project-Zero_2DPlatformer/Assets/Scripts/Player_Bullet.cs
@@ -20,16 +20,7 @@ public class Player_Bullet : MonoBehaviour {
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-
-        if (collision.gameObject.tag != "Bullet" || collision.gameObject.tag != "Player")
-        {
-            Instantiate(impactEffect, transform.position, transform.rotation);
-            // HUOM! Objecti tuhotaan palapalata, jotta partikkeli efekti ei tuhoutuisi mukana. 
-            Destroy(GetComponent<Rigidbody>());
-            Destroy(GetComponent<BoxCollider2D>());
-            Destroy(GetComponent<SpriteRenderer>());
-            Destroy(gameObject, 1);
-        }
+   
         if (collision.gameObject.tag == "Enemy")
         {
             Enemy enemy = collision.gameObject.GetComponent<Enemy>();
@@ -41,7 +32,16 @@ public class Player_Bullet : MonoBehaviour {
                 enemy.TakeDamage(damage);
             }
         }
-       
+
+        if (collision.gameObject.tag != "Bullet" || collision.gameObject.tag != "Player")
+        {
+            Instantiate(impactEffect, transform.position, transform.rotation);
+            // HUOM! Objecti tuhotaan palapalalta, jotta partikkeli efekti ei tuhoutuisi mukana. 
+            Destroy(GetComponent<Rigidbody>());
+            Destroy(GetComponent<BoxCollider2D>());
+            Destroy(GetComponent<SpriteRenderer>());
+            Destroy(gameObject, 1);
+        }
     }
 
     void OnBecameInvisible()
