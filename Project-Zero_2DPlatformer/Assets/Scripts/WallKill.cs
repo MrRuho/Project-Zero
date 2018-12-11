f291e38b380ab7620cd1f71f81293c0b4e5dc15f
Paajanen Ville <paajanen.ville@gmail.com>
2018-12-11 17:39:23 +0200

    Testikenttä. WallKill toimii.
    - (Todo) Kamera oikeammalle.
    - (Todo) Peli ei nollaannu kun hahmo kuolee vaan jatkuu uudella hahmolla.

--- Project-Zero_2DPlatformer/Assets/Scripts/WallKill.cs
+++ Project-Zero_2DPlatformer/Assets/Scripts/WallKill.cs
@@ -4,13 +4,31 @@ using UnityEngine;
 
 public class WallKill : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private float timer = 0f;
+    private Player player;
+    // Use this for initialization
+    void Start ()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Ground"))
+        { 
+            timer += Time.deltaTime;  
+        }
+        if (timer >= 0.5f)
+        {
+            player.Damage(5);  
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Ground"))
+        {
+            timer = 0f;
+        }
+    }
 }
