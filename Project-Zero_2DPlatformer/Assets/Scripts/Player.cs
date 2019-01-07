be4be2e768da84e9e259c0ee2a1f8cacb09fa319
MrRuho <paajanen.ville@gmail.com>
2019-01-07 15:52:18 +0200

    pelaaja asennettu

--- Project-Zero_2DPlatformer/Assets/Scripts/Player.cs
+++ Project-Zero_2DPlatformer/Assets/Scripts/Player.cs
@@ -48,8 +48,8 @@ public class Player : MonoBehaviour {
         capsuleCollider2D = gameObject.GetComponent<CapsuleCollider2D>();
         if (capsuleCollider2D != null)
         {
-            capsuleCollider2D.size = new Vector3(0.58f, 1f, 0);
-            capsuleCollider2D.offset = new Vector3(0.14f, 0.06f, 0);
+            capsuleCollider2D.size = new Vector3(0.34f, 1.2f, 0);
+            capsuleCollider2D.offset = new Vector3(0.08f, -0.04f, 0);
             capsuleCollider2D.direction = CapsuleDirection2D.Vertical;
         }
 
@@ -167,8 +167,8 @@ public class Player : MonoBehaviour {
     // Slide animaation loputtua Animation kaynnistaa taman eventin.
     void SlideEnds()
     {
-        capsuleCollider2D.size = new Vector3(0.58f, 1f, 0);
-        capsuleCollider2D.offset = new Vector3(0.14f, 0.06f, 0);
+        capsuleCollider2D.size = new Vector3(0.9f, 0.5f, 0);
+        capsuleCollider2D.offset = new Vector3(0, -0.19f, 0);
         capsuleCollider2D.direction = CapsuleDirection2D.Vertical;
         anim.SetBool("Slide", false);
         sliding = false;
