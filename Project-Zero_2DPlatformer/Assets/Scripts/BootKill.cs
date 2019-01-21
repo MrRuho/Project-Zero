09eb7acfc28a073113688e71823cdeb019b5bf04
MrRuho <paajanen.ville@gmail.com>
2019-01-21 15:19:44 +0200

    CheckPointti toimii
    Zombien boxCollideri häviää kolmannella osumakerralla.

--- Project-Zero_2DPlatformer/Assets/Scripts/BootKill.cs
+++ Project-Zero_2DPlatformer/Assets/Scripts/BootKill.cs
@@ -4,17 +4,9 @@ using UnityEngine;
 
 public class BootKill : MonoBehaviour
 {
-    
-    private Player player;
-
-    private void Start()
-    {
-        player = gameObject.GetComponentInParent<Player>();
-    }
 
     private void OnTriggerEnter2D(Collider2D collision)
-    {
-        
+    {       
         if (collision.gameObject.tag == "Enemy" && Player.sliding == true)
         {
             Enemy enemy = collision.gameObject.GetComponent<Enemy>();
---
+++ Project-Zero_2DPlatformer/Assets/Scripts/checkPoint.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class checkPoint : MonoBehaviour{
+
+    bool playerHasReachedThisCheckpoint = false;
+    public Transform newPlayerSpawnPoint;
+    public GameObject playerSpawnPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && playerHasReachedThisCheckpoint == false)
+        {
+            playerHasReachedThisCheckpoint = true;
+            Destroy(GameObject.FindGameObjectWithTag("PlayerSpawnPoint"));
+            Instantiate(playerSpawnPoint, newPlayerSpawnPoint.position, newPlayerSpawnPoint.rotation);
+        }
+    }
+}
