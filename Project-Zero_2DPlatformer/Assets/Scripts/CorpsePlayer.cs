64323b6b8cbcd047b245688115ae9681310e8716
MrRuho <paajanen.ville@gmail.com>
2019-01-23 15:21:25 +0200

    Pelaaja lentää kun osuu viholliseen (työnalla)

--- Project-Zero_2DPlatformer/Assets/Scripts/CorpsePlayer.cs
+++ Project-Zero_2DPlatformer/Assets/Scripts/CorpsePlayer.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class CorpsePlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody2D rb2d;
+    private float force = 800.0f;
+        // Start is called before the first frame update
     void Start()
     {
-        //Physics2D.IgnoreLayerCollision(12, 11);
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+        rb2d.AddForce(Vector2.right * force);
+        Physics2D.IgnoreLayerCollision(12, 11);
+        Physics2D.IgnoreLayerCollision(12, 0);
     }
 
     // Update is called once per frame
@@ -16,7 +21,7 @@ public class CorpsePlayer : MonoBehaviour
       
     }
 
-    void OnCollisionStay(Collision collide)
+    void OnCollisionEnter(Collision collide)
     {
         //Output the name of the GameObject you collide with
         Debug.Log("I hit the GameObject : " + collide.gameObject.name);
