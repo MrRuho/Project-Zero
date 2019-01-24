f3c860693e0a96e4771701cbeb7eca76ff870a5e
MrRuho <paajanen.ville@gmail.com>
2019-01-24 15:42:30 +0200

    vihut spownaantuu (työvaiheessa) + hyppy kuva pelaajasta

---
+++ Project-Zero_2DPlatformer/Assets/Scripts/CreateEnemysPoint.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateEnemysPoint : MonoBehaviour
+{
+    public static bool PlayerHasEnteredZone = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && PlayerHasEnteredZone == false)
+        {
+            PlayerHasEnteredZone = true;
+            Debug.Log(" PlayerHasEnteredZone " + PlayerHasEnteredZone);
+        }
+    }
+}
