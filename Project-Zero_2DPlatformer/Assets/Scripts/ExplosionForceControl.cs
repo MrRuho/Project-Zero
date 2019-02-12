38c9a204aaa33d8dd322304859c48994382481dd
MrRuho <paajanen.ville@gmail.com>
2019-02-12 14:27:51 +0200

    Ohjuksessa on räjähdys forse

---
+++ Project-Zero_2DPlatformer/Assets/Scripts/ExplosionForceControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionForceControl : MonoBehaviour
+{
+    public float delayTime = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(delay());     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator delay() {
+        yield return new WaitForSeconds(delayTime);
+        DestroyComponent();
+        yield return 0;
+    }
+
+    void DestroyComponent()
+    {
+        Destroy(GetComponent<PointEffector2D>());
+    }
+
+}
