cf25aa7c6edc0612b39265125656203f57c3495d
MrRuho <paajanen.ville@gmail.com>
2019-02-13 15:30:49 +0200

    räjähdykset lennättääja hajottaa zombiet

---
+++ Project-Zero_2DPlatformer/Assets/Scripts/RandomDestroyer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomDestroyer : MonoBehaviour
+{
+    int randomDestroyer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        randomDestroyer = Random.Range(1, 10);
+        if (randomDestroyer == 1) {
+            Destroy(gameObject, 0.05f);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
