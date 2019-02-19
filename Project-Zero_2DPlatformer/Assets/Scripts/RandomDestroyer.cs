afac5150a5f193a53523b5bda9e61309a4eb57aa
MrRuho <paajanen.ville@gmail.com>
2019-02-19 14:19:41 +0200

    Kohta jossa koodia siivottu paluukuonasta. Tuho Zombie korpputoitunut.

--- Project-Zero_2DPlatformer/Assets/Scripts/RandomDestroyer.cs
+++ Project-Zero_2DPlatformer/Assets/Scripts/RandomDestroyer.cs
@@ -3,77 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RandomDestroyer : MonoBehaviour
-{
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
+{ 
+
     private int randomDestroyer;
     private int destroyByOverWalking = 1;
     
-
-=======
-    int randomDestroyer;
->>>>>>> parent of 5b821bd... ruumiin päälle tallominen työnalla
     public int maxRandomRangeRifle = 0;
     public int maxRandomRangeShotGun = 0;
     public int maxRandomRangeMissile = 0;
-    // Start is called before the first frame update
-    void Start()
-    {
-        if (Player_Weapons.weapon == 3) // missile
-=======
-    int randomDestroyer;
-    // Start is called before the first frame update
-    void Start()
-    {
-        if (Player_Weapons.weapon == 3)
->>>>>>> parent of 9df1d5f... zombeilla on vipuvarsi
-=======
-    int randomDestroyer;
-    // Start is called before the first frame update
-    void Start()
-    {
-        if (Player_Weapons.weapon == 3)
->>>>>>> parent of 9df1d5f... zombeilla on vipuvarsi
-        {
-            randomDestroyer = Random.Range(1, 3);
-        } else {
-            randomDestroyer = Random.Range(1, 10);
-        }
+ 
+    void Start() {
+    if (Player_Weapons.weapon == 3) {
 
-        if (randomDestroyer == 1) {
-            Destroy(gameObject, 0.05f);
+        randomDestroyer = Random.Range(1, 3);
+    } else {
+
+        randomDestroyer = Random.Range(1, 10);
+    }
+    if (randomDestroyer == 1){
+
+        Destroy(gameObject, 0.05f);
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-    private void Destroy()
-    {
+    void Update(){
+
+    }
+
+
+    private void Destroy(){
+
         Destroy(gameObject, 0.05f);
     }
-<<<<<<< HEAD
 
-    void OnTriggerEnter2D()
-    {
+    void OnTriggerEnter2D(){
+
         destroyByOverWalking++;
-        if (destroyByOverWalking < 0)
-        {
+
+        if (destroyByOverWalking < 0){
             Debug.Log("ruumis tuhoutuu");
             Destroy(gameObject);
         }
 
-    }
-=======
->>>>>>> parent of 9df1d5f... zombeilla on vipuvarsi
-=======
->>>>>>> parent of 9df1d5f... zombeilla on vipuvarsi
-=======
->>>>>>> parent of 5b821bd... ruumiin päälle tallominen työnalla
+    }   
 }
